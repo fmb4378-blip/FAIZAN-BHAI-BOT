263739a8b98bd4a5df1f8de0599eee04ed5ceafd
fmb4378-blip <fmb4378@gmail.com>
2026-08-14 15:14:50 +0500

    config.exapmle.js

---
+++ config.exapmle.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mainToken: 'PASTE_YOUR_BOT_TOKEN_HERE',
+    adminId: 'PASTE_YOUR_TELEGRAM_ID',
+    bot: 'Xeon Bug Bot Strong 2026',
+    S7: '@yourname',
+    channel: '', group: '', youtube: '', waChannel: '', instagram: '',
+    channelId: '', groupId: '',
+    logo: './SY/Loves.jpg'
+};
