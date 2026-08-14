70372334e742289c6a56354c972f7b91db9abba6
fmb4378-blip <fmb4378@gmail.com>
2026-08-14 15:12:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,50 +1,9 @@
-/*
- * © 2026 SeXyxeon (VOIDSEC)
- *
- * ⚠️ COPYRIGHT NOTICE
- * This source code is protected under copyright law.
- * Any form of re-uploading, recoding, modification,
- * selling, or redistribution WITHOUT explicit permission
- * from the original author is strictly prohibited.
- *
- * ❌ NO CREDIT = NO PERMISSION
- * ❌ DO NOT CLAIM THIS CODE AS YOUR OWN
- *
- * ✔️ Usage or modification is allowed ONLY
- * with prior permission and proper credit.
- *
- * OFFICIAL LINKS (ONLY):
- * YouTube   : https://youtube.com/@voidsec7718
- * Instagram : sabir._7718
- * Telegram  : https://t.me/SABIR7718
- * GitHub    : https://github.com/SABIR7718
- * WhatsApp  : +91 73650 85213
- *
- * Violations may result in DMCA takedown
- * or termination of the Telegram bot.
- */
 module.exports = {
-    mainToken: 'BOT_TOKEN', // Bot Token Here 
-    
-    S7: '@SeXyxeon13', // Owner, Name
-    
-    adminId: 'CHAT_ID', // Owner Chat ID ( control /address, /delresell )
-    
-    channel: 'TG_CHANNEL', // TG channel link
-    
-    group: 'GC_LINK', // TG group link
-    
-    youtube: 'YOITUBE_LINK', // YouTube Link
-    
-    waChannel: 'WA_LINK', // WhatsApp Channel Link
-    
-    instagram: 'INSTA_LINK', // Instagram Link 
-    
-    channelId: 'CHANNEL_ID', // channel ID bot must be admin in the channel
-    
-    groupId: 'GROUP_ID', // group ID bot must be admin in the group
-    
-    bot: 'Xeon Bug Bot V27', // Bot name 
-    
-    logo: './SY/Loves.jpg' // Bot logo path or URL
+    mainToken: '8972896713:AAGMzV3Gjw999jom7xoU9CV2TyL-fNP65-s',
+    adminId: '6736708463',
+    bot: 'Xeon Bug Bot Strong 2026',
+    S7: '@yourname',
+    channel: '', group: '', youtube: '', waChannel: '', instagram: '',
+    channelId: '', groupId: '',
+    logo: './SY/Loves.jpg'
 };
