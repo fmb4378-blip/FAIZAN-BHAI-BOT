08baf116884a76017ff0713aaed1c3952be0e325
fmb4378-blip <fmb4378@gmail.com>
2026-08-14 15:24:18 +0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mainToken: '8972896713:AAGMzV3Gjw999jom7xoU9CV2TyL-fNP65-s',
-    adminId: '6736708463',
-    bot: 'Xeon Bug Bot Strong 2026',
-    S7: '@yourname',
-    channel: '', group: '', youtube: '', waChannel: '', instagram: '',
-    channelId: '', groupId: '',
-    logo: './SY/Loves.jpg'
-};
